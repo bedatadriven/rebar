2fa3ba1759433ebb63043a6a160e79251e9c4d3d
Alex Bertram <alex@bedatadriven.com>
2011-08-09 09:43:27 +0200

    Initial import of dao generator maven plugin

---
+++ dao-test/src/main/java/com/bedatadriven/rebar/dao/test/AdminEntity.java
@@ -0,0 +1,8 @@
+package com.bedatadriven.rebar.dao.test;
+
+public class AdminEntity {
+
+  private int id;
+  private String name;
+
+}
---
+++ dao/src/test/java/com/bedatadriven/rebar/dao/GeneratorTest.java
@@ -0,0 +1,49 @@
+package com.bedatadriven.rebar.dao;
+
+import org.junit.Ignore;
+
+@Ignore
+public class GeneratorTest {
+//
+//	private Configuration cfg;
+//
+//	@Before
+//	public void setUpTemplates() {
+//		cfg = new Configuration();
+//		// Specify the data source where the template files come from.
+//		// Here I set a file directory for it:
+//		cfg.setClassForTemplateLoading(getClass(), "");
+//		cfg.setObjectWrapper(new DefaultObjectWrapper());
+//	}
+//
+//	@Test
+//	public void parse() throws JAXBException, IOException, TemplateException {
+//
+//		DaoModel model = parseModel();
+//
+//		Assert.assertEquals(FieldType.TEXT, model.getTableByName("Indicator").getColByName("name").getType());
+//
+////		printTable(model.getTableByName("Indicator"), "CreateTable.sql.ftl");
+////		printView(model.getViewByName("AdminEntity"), "ViewModelData.java.ftl");
+////		printView(model.getViewByName("AdminEntity"), "SyncDAO.java.ftl");
+//
+//	}
+//
+//	private void printView(View view, String templateName) throws IOException, TemplateException {
+//		Template temp = cfg.getTemplate(templateName);
+//		temp.process(view, new PrintWriter(System.out));
+//	}
+//
+//	private void printTable(Table table, String templateName) throws IOException, TemplateException {
+//		Template temp = cfg.getTemplate(templateName);
+//		temp.process(table, new PrintWriter(System.out));
+//	}
+//
+//	private DaoModel parseModel() throws JAXBException {
+//		JAXBContext jc = JAXBContext.newInstance(DaoModel.class);
+//		Unmarshaller unmarshaller = jc.createUnmarshaller();
+//		DaoModel model = (DaoModel) unmarshaller.unmarshal(getClass().getResourceAsStream("/test.xml"));
+//		return model;
+//	}
+
+}
