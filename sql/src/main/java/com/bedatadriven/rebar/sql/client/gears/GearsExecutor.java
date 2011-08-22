be2d836b08dcb4be26b85229f33f1fd96e379ccd
alex@bedatadriven.com <alex@bedatadriven.com>
2011-08-22 22:53:43 +0200

    Added workaround for gears bug where 'boxed' js strings are not properly handled and result in 'SQL paramter X has unknown type'

--- sql/src/main/java/com/bedatadriven/rebar/sql/client/gears/GearsExecutor.java
+++ sql/src/main/java/com/bedatadriven/rebar/sql/client/gears/GearsExecutor.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import com.bedatadriven.rebar.sql.client.SqlException;
 import com.bedatadriven.rebar.sql.client.SqlResultSet;
 import com.bedatadriven.rebar.sql.shared.adapter.SyncTransactionAdapter;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.gears.client.Factory;
 import com.google.gwt.gears.client.database.Database;
 import com.google.gwt.gears.client.database.DatabaseException;
@@ -62,30 +63,12 @@ class GearsExecutor implements SyncTransactionAdapter.Executor {
 
   @Override
   public SqlResultSet execute(String statement, Object[] params) throws Exception {
-    ResultSet rs = db.execute(statement, toStringArray(params));
+    ResultSet rs = execute(db, statement, ParamArray.create(params));
     try {
       return new SqlResultSet(db.getLastInsertRowId(), db.getRowsAffected(), 
       		new GearsResultSetRowList(rs));
     } finally {
       try { rs.close(); } catch(Exception ignored) {}
-    }
-  }
-
-  private String[] toStringArray(Object[] params) {
-    if(params == null) {
-      return new String[0];
-    } else {
-      String[] s = new String[params.length];
-      for(int i=0;i!=params.length;++i) {
-      	if(params[i] == null) {
-      		s[i] = null;
-      	} else if(params[i] instanceof Date) {
-      		s[i] = Long.toString(((Date)params[i]).getTime());
-      	} else {
-      		s[i] = params[i].toString();
-      	}
-      }
-      return s;
     }
   }
 
@@ -107,5 +90,50 @@ class GearsExecutor implements SyncTransactionAdapter.Executor {
   		transactionInProgress = false;
   		db.close();
   	}	  
+  }
+	
+	private native ResultSet execute(Database db, String sqlStatement, JavaScriptObject args) /*-{
+  	return db.execute(sqlStatement, args);
+	}-*/;
+
+  /**
+   * There is a slight bug in the gears C++ implementation that results
+   * in an 'SQL paramter X has unknown type' because it does not handle 
+   * 'boxed' JavaScript strings.
+   * 
+   * This class creates an array of primtive strings that the gears
+   * module can handle without problems.
+   */
+  private final static class ParamArray extends JavaScriptObject {
+  	
+  	protected ParamArray() {	
+  	}
+  	
+  	private static native ParamArray create() /*-{
+  		return [];
+  	}-*/;
+  	
+  	public static ParamArray create(Object[] values) {
+  		ParamArray array = create();
+  		if(values != null) {
+	  		for(int i=0;i!=values.length;++i) {
+	  			Object value = values[i];
+	  			if(value instanceof Date) {
+	    			array.set(i, (double)((Date) value).getTime());  				
+	  			} else if(value instanceof Long) {
+	  				array.set(i, ((Long) value).doubleValue());
+	  			} else {
+	  				array.set(i, value);
+	  			}
+	  		}
+  		}
+  		return array;
+  	}
+
+  	// cast the value explicitly to a primitive js string that
+  	// gears can understand with String()
+  	public native void set(int i, Object value) /*-{
+  		this[i] = String(value);
+  	}-*/;
   }
 }
