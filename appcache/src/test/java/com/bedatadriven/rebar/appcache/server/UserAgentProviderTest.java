e4390b4cc2adf2cd5665ed8e812a3cb13bb8accb
alex@bedatadriven.com <alex@bedatadriven.com>
2011-08-22 11:01:39 +0200

    additional user agent sniffing tests (they were ok)

--- appcache/src/test/java/com/bedatadriven/rebar/appcache/server/UserAgentProviderTest.java
+++ appcache/src/test/java/com/bedatadriven/rebar/appcache/server/UserAgentProviderTest.java
@@ -19,6 +19,13 @@ public class UserAgentProviderTest {
   }
 
   @Test
+  public void testIE8Windows() {
+    assertThat(devineUserAgent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; GTB7.1; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; eSobiSubscriber 2.0.4.16; .NET4.0C"), 
+   		 equalTo("ie8"));
+   
+  }
+  
+  @Test
   public void testChrome() {
     
     assertThat(devineUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1 Paros/3.2.13"), 
