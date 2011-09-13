35dc4e03136c1b714f2339b501c96552a8ab15c8
Alex Bertram <alex@bedatadriven.com>
2011-09-13 08:07:43 +0200

    Trying to work around flaky Gears Workerpool by treating errors as transient and rebuilding pool after failures and retrying. Also getting rid of boxed Integer's all together.

--- sql/src/main/java/com/bedatadriven/rebar/sql/client/gears/GearsBulkUpdater.java
+++ sql/src/main/java/com/bedatadriven/rebar/sql/client/gears/GearsBulkUpdater.java
@@ -19,8 +19,6 @@ package com.bedatadriven.rebar.sql.client.gears;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.mortbay.util.ajax.JSON;
-
 import com.allen_sauer.gwt.log.client.Log;
 import com.bedatadriven.rebar.sql.client.gears.worker.WorkerCommand;
 import com.bedatadriven.rebar.sql.client.gears.worker.WorkerResponse;
@@ -28,8 +26,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.gears.client.Factory;
 import com.google.gwt.gears.client.workerpool.WorkerPool;
 import com.google.gwt.gears.client.workerpool.WorkerPoolMessageHandler;
-import com.google.gwt.json.client.JSONParser;
-import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
@@ -41,91 +37,103 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 class GearsBulkUpdater implements WorkerPoolMessageHandler {
 
 	public static final GearsBulkUpdater INSTANCE = new GearsBulkUpdater();
-	
-  private WorkerPool pool;
-  private Integer workerId;
-  private int nextExecutionId = 1;
-  private Map<Integer, AsyncCallback> callbacks = new HashMap<Integer, AsyncCallback>();
-  
-  private GearsBulkUpdater() {
-  	
+
+	private WorkerPool pool;
+	private int workerId = -1;
+	private int nextExecutionId = 1;
+	private Map<Integer, AsyncCallback> callbacks = new HashMap<Integer, AsyncCallback>();
+
+	private GearsBulkUpdater() {
+
+	}
+
+	public void executeUpdates(String databaseName, String bulkOperationJsonArray, AsyncCallback<Integer> callback) {
+		try {
+			int executionId = nextExecutionId++;
+			Log.trace("GearsBulkUpdater: starting executeUpdates() for executionId=" + executionId);
+
+			// Create our worker if we haven't already
+			if(pool == null) {
+				initPool();
+			}
+
+			// Construct our message
+			Log.trace("GearsBulkUpdater: constructing message");
+
+			String command = WorkerCommand.newCommandAsJson(executionId, databaseName, bulkOperationJsonArray);
+
+			// Register our callback
+			callbacks.put(executionId, callback);
+
+			// Dispatch our command to the worker
+			try {
+				sendMessage(command);    
+			} catch(Throwable t) {
+				// this is failing inexplicably in IE on second attempts to synchronize,
+				// let's try reinitializng the pool and reloading the worker
+				initPool();
+				sendMessage(command);
+			}
+			
+			Log.trace("GearsBulkUpdater: sent message to worker");
+		} catch(Throwable e) {
+			Log.debug("GearsBulkUpdater: exception thrown while sending message: " + e.getMessage(), e);
+			callback.onFailure(e);
+		}
+	}
+
+	private void sendMessage(String command) {
+	  Log.trace("GearsBulkUpdater: about to send message to worker " + workerId);
+	  sendMessageSafe( pool, command, workerId );
   }
 
-  public void executeUpdates(String databaseName, String bulkOperationJsonArray, AsyncCallback<Integer> callback) {
-    try {
-	  	int executionId = nextExecutionId++;
-	  	Log.trace("GearsBulkUpdater: starting executeUpdates() for executionId=" + executionId);
-	    
-	    // Create our worker if we haven't already
-	    if(pool == null) {
-	      pool = Factory.getInstance().createWorkerPool();
-	      pool.setMessageHandler(this);
-	      workerId = pool.createWorkerFromUrl(GWT.getModuleBaseURL() +
-	          "GearsSqlWorker.js");
-	      
-	      Log.debug("GearsBulkUpdater: Created worker pool, workerId = " + workerId);
-	    }
-	
-	    // Construct our message
-    	Log.trace("GearsBulkUpdater: constructing message");
+	private void initPool() {
+	  pool = Factory.getInstance().createWorkerPool();
+	  pool.setMessageHandler(this);
+	  workerId = pool.createWorkerFromUrl(GWT.getModuleBaseURL() +
+	  		"GearsSqlWorker.js");
 
-    	String command = WorkerCommand.newCommandAsJson(executionId, databaseName, bulkOperationJsonArray);
-
-	    // Register our callback
-	    callbacks.put(executionId, callback);
-	
-	    // Dispatch our command to the worker
-    	Log.trace("GearsBulkUpdater: about to send message to worker " + workerId);
-
-    	sendMessageSafe( pool, command, workerId.intValue() );    
-    	
-    	Log.trace("GearsBulkUpdater: sent message to worker");
-    } catch(Throwable e) {
-    	Log.debug("GearsBulkUpdater: exception thrown while sending message: " + e.getMessage(), e);
-    	callback.onFailure(e);
-    }
+	  Log.debug("GearsBulkUpdater: Created worker pool, workerId = " + workerId);
   }
 
-  public void onMessageReceived(MessageEvent messageEvent) {
-  	 	
-  	WorkerResponse response;
-  	try {
-  		response =  WorkerResponse.parse( messageEvent.getBody() );
-  	} catch(Throwable t) {
-  		Log.debug("GearsBulkUpdater: exception parsing worker response. response = " + messageEvent.getBody()
-  					+ ", exception = " + t.getMessage());
-  		return;
-  	}
+	public void onMessageReceived(MessageEvent messageEvent) {
 
-    if(response.getType() == WorkerResponse.LOG) {
-      // Log message from Worker
-      Log.error("WorkerBulkExecutor[" + response.getExecutionId() + "] : " + response.getMessage());
+		WorkerResponse response;
+		try {
+			response =  WorkerResponse.parse( messageEvent.getBody() );
+		} catch(Throwable t) {
+			Log.debug("GearsBulkUpdater: exception parsing worker response. response = " + messageEvent.getBody()
+					+ ", exception = " + t.getMessage());
+			return;
+		}
 
-    } else {
-      // Find the callback for this execution
-      AsyncCallback<Integer> callback = callbacks.get(response.getExecutionId());
+		if(response.getType() == WorkerResponse.LOG) {
+			// Log message from Worker
+			Log.error("WorkerBulkExecutor[" + response.getExecutionId() + "] : " + response.getMessage());
 
-      if(response.getType() == WorkerResponse.EXCEPTION) {
-        Log.error("GearsBulkUpdater[" + response.getExecutionId() + "] : Exception thrown during execution: " + response.getMessage());
-        callback.onFailure(new Exception(response.getMessage()));
-        callbacks.remove(response.getExecutionId());
+		} else {
+			// Find the callback for this execution
+			AsyncCallback<Integer> callback = callbacks.get(response.getExecutionId());
 
-      } else if(response.getType() == WorkerResponse.SUCCESS) {
-        Log.debug("GearsBulkUpdater[" + response.getExecutionId() + "] : Completed successfully: " +
-            response.getRowsAffected() + " row(s) affected");
+			if(response.getType() == WorkerResponse.EXCEPTION) {
+				Log.error("GearsBulkUpdater[" + response.getExecutionId() + "] : Exception thrown during execution: " + response.getMessage());
+				callback.onFailure(new Exception(response.getMessage()));
+				callbacks.remove(response.getExecutionId());
 
-        callback.onSuccess(response.getRowsAffected());
-        callbacks.remove(response.getExecutionId());
+			} else if(response.getType() == WorkerResponse.SUCCESS) {
+				Log.debug("GearsBulkUpdater[" + response.getExecutionId() + "] : Completed successfully: " +
+						response.getRowsAffected() + " row(s) affected");
 
-      } else {
-        Log.error("WorkerBulkExecutor: Unknown response type = " + response.getType());
-      }
-    }
-  }
-  
+				callback.onSuccess(response.getRowsAffected());
+				callbacks.remove(response.getExecutionId());
+
+			} else {
+				Log.error("WorkerBulkExecutor: Unknown response type = " + response.getType());
+			}
+		}
+	}
+
 	private static native void sendMessageSafe(WorkerPool pool, String message, int workerId) /*-{
 		pool.sendMessage(String(message), Number(workerId));
 	}-*/;
-
-  
 }
