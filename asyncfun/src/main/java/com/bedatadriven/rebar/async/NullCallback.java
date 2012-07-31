414a73655834722bea0087576ce9a6e27c00caa1
Alex Bertram <alex@bedatadriven.com>
2012-07-31 08:29:39 +0200

    Adds convenience static contstructors for NullCallback

--- asyncfun/src/main/java/com/bedatadriven/rebar/async/NullCallback.java
+++ asyncfun/src/main/java/com/bedatadriven/rebar/async/NullCallback.java
@@ -1,7 +1,4 @@
-/*
- * All Sigmah code is released under the GNU General Public License v3
- * See COPYRIGHT.txt and LICENSE.txt.
- */
+
 
 package com.bedatadriven.rebar.async;
 
@@ -9,9 +6,17 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class NullCallback<T> implements AsyncCallback<T> {
 
-    @Override
-    public void onFailure(Throwable throwable) { /* NO OP */ }
+	@Override
+	public void onFailure(Throwable throwable) { /* NO OP */ }
 
-    @Override
-    public void onSuccess(T t) { /* NO OP */ }
+	@Override
+	public void onSuccess(T t) { /* NO OP */ }
+
+	public static <T> NullCallback<T> create() {
+		return new NullCallback<T>();
+	}
+
+	public static NullCallback<Void> forVoid() {
+		return new NullCallback<Void>();
+	}
 }
